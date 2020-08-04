8dfeac13afa0ce6ef7df8b00b3c275f637715a39
Obandoem <50923793+obandoem@users.noreply.github.com>
2020-08-04 09:42:46 -0600

    Update AreaTriangulo.java

---
+++ Semana1/AreaTriangulo.java
@@ -0,0 +1,8 @@
+//Para  calcular  el  área  de  un  triángulo  se  multiplica  la  medida  de  su  base  por  su  altura  y  se divide entre dos. 
+//Diseñe un algoritmo que reciba como entrada la medida de la base  y la altura del triángulo. Debe calcular y retornar el área del mismo.
+
+void areaTriangulo (float base, float altura) {
+    float area;
+    area = base*(altura/2);
+    System.out.println("El area del triangulo es de " + area);
+}

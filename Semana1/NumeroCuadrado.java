6a3d7bd3481fa742bae001110cc158eeba503c4d
Emeth Obando <emethobando@pop-os.localdomain>
2020-08-04 09:33:33 -0600

    first commit

---
+++ Semana1/NumeroCuadrado.java
@@ -0,0 +1,9 @@
+//Diseñe  un  algoritmo  que  reciba  como  entrada  un  número  e  imprima  el  cuadrado  y  la  raíz cuadrada de éste.
+
+void numeroCuadrado (float numero) {
+    float raiz, cuadrado;
+    raiz = sqrt(numero);
+    cuadro = pow(numero);
+    System.out.println("La raiz cuadrada de " + numero + " es de " + raiz);
+    System.out.println("El cuadrado de " + numero + " es de " + cuadro);
+}

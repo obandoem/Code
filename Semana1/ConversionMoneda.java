a17c081b362f9ca424c121bfb802eadee3ff2559
Obandoem <50923793+obandoem@users.noreply.github.com>
2020-08-04 15:57:51 -0600

    Delete ConversionMoneda.java

--- Semana1/ConversionMoneda.java
+++
@@ -1,9 +0,0 @@
-//Diseñe  un  algoritmo  que  reciba  como  entrada  un  valor  numérico  que  representará  una cantidad en colones, 
-//convierta dicha cantidad a dólares, sabiendo que el tipo de cambio es de 545 colones por dólar.
-
-void conversionMoneda (float colones){
-    final int CAMBIO = 545;
-    float conversion;
-    conversion = colones/CAMBIO;
-    System.out.println("La conversion de " + colones + " a dolares es de: " + conversion);
-}
